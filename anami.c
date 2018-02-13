747814914445710092dd30eebe20515079e9a1c7
sowbarnica <31504825+sowbarnica@users.noreply.github.com>
2018-02-13 14:48:56 +0530

    Update and rename answer.c to anami.c

--- anami.c
+++ anami.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-	int s,d,v,ans;
+	int s,d,v,answer;
 	scanf("%d%d%d",&s,&d,&v);
 	printf("Enter the three numbers %d %d  %d\n",s,d,v);
-	ans=(s^d)%v;
-	printf("The value is %d\n",ans);
+	answer=(s^d)%v;
+	printf("The value is %d\n",answer);
 	return 0;
 }
+
