b06469eac9e216232b2e3cbe31ce59cadc1e80d8
sowbarnica <31504825+sowbarnica@users.noreply.github.com>
2018-03-02 10:20:39 +0000

    Create hrs.c

---
+++ hrs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	int v,s,d,hr1,min1,hr2,min2;
+	scanf("%d%d%d%d",&hr1,&hr2,&min1,&min2);
+	s=hr1:min1;
+	d=hr2:min2;
+	printf("Enter the two hours and minutes:%d %d\n",s,d);
+v=s-d;
+printf("Enter the calculated hour and minute:%d \n",v);
+	return 0;
+}
