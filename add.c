088edaa21a16924e128b64aeb91c66eddc3049f6
sowbarnica <31504825+sowbarnica@users.noreply.github.com>
2018-03-02 10:45:43 +0000

    Create add.c

---
+++ add.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	int v,d,s;
+	scanf("%d%d",&d,&s);
+	printf("Enter two numbers:%d %d\n",d,s);
+	v=d+s;
+	printf("Enter the added value:%d\n",v);
+	return 0;
+}
