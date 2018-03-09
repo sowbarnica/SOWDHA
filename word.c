d766e5f0161de25639b1687b9e6d301db20b90a3
sowbarnica <31504825+sowbarnica@users.noreply.github.com>
2018-03-09 10:46:42 +0000

    Create word.c

---
+++ word.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+	int d;
+	scanf("%d",&d);
+	switch(d)
+	{
+		case 1:
+		printf("one\n",d);break;
+		case 2:
+		printf("two\n",d);break;
+		case 3:
+		printf("three\n",d);break;
+		case 4:
+		printf("four\n",d);break;
+		case 5:
+		printf("five\n",d);break;
+		case 6:
+		printf("six\n",d);break;
+		case 7:
+		printf("seven\n",d);break;
+		case 8:
+		printf("eight\n",d);break;
+		case 9:
+		printf("nine\n",d);break;
+		case 10:
+		printf("ten\n",d);break;
+	}
+	return 0;
+}
